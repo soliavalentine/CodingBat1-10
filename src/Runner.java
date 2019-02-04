c0c728531259d0fb4c8f7fa2cbdb24ed94c6feb3
Solia Valentine <SoliaValentine@students.berkeley.net>
2019-02-04 13:33:22 -0800

    Finished coding bat 1-10

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,25 @@
-public class CodingBat {
+public class Runner {
+    public static void main(String[] args) {
+        int[] x = {2,3,4};
+        System.out.println(makes10(1,2));
+        System.out.println(doubleX("xxxyyyy"));
+        System.out.println(helloName("Maria"));
+        System.out.println(no23(x));
+        System.out.println(cigarParty(69,true));
+        System.out.println(noTeenSum(14,15,69));
+        System.out.println(countHi("hihihihihi"));
+        System.out.println(sumDigits("12hdsjkhckw253"));
+        System.out.println(lucky13(x));
+        System.out.println(maxSpan(x));
+    }
     //Warmup-1 makes10
-    public boolean makes10(int a, int b) {
+    public static boolean makes10(int a, int b) {
         if(a==10 ||b==10 || a+b==10){
             return true;
         }else return false;
     }
     //Warmup-2 doubleX
-    boolean doubleX(String str) {
+    public static boolean doubleX(String str) {
         int x = str.indexOf("x");
         if (x == -1){
             return false;
@@ -18,7 +31,7 @@ public class CodingBat {
         return str.substring(x+1, x+2).equals("x");
     }
     //String-1 helloName
-    public String helloName(String name) {
+    public static String helloName(String name) {
         return("Hello " + name + "!");
     }
     //Array-1 biggerTwo
@@ -29,8 +42,18 @@ public class CodingBat {
             return b;
         }
     }
+    //Array-1 no23
+    public static boolean no23(int[] nums) {
+        boolean a = true;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]==2 || nums[i]==3){
+                a=false;
+            }
+        }return a;
+    }
+
     //Logic-1 cigarParty
-    public boolean cigarParty(int cigars, boolean isWeekend) {
+    public static boolean cigarParty(int cigars, boolean isWeekend) {
         if(isWeekend == true && cigars>=40 || isWeekend== false && cigars>=40 && cigars<=60){
             return true;
         }else {
@@ -38,7 +61,7 @@ public class CodingBat {
         }
     }
     //Logic-2 noTeenSum
-    public int noTeenSum(int a, int b, int c) {
+    public static int noTeenSum(int a, int b, int c) {
         if(a>=13 && a<=19 && a!=15 && a!=16){
             a = 0;
         }if(b>=13 && b<=19 && b!=15 && b!=16){
@@ -48,7 +71,7 @@ public class CodingBat {
         }return(a+b+c);
     }
     //String-2 countHi
-    public int countHi(String str) {
+    public static int countHi(String str) {
         int num = 0;
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.substring(i, i + 2).equals("hi")) num++;
@@ -56,7 +79,7 @@ public class CodingBat {
         return num;
     }
     //String-3 sumDigits
-    public int sumDigits(String str) {
+    public static int sumDigits(String str) {
         int sum = 0;
         for(int i = 0; i < str.length(); i++){
             if(Character.isDigit(str.charAt(i))){
@@ -65,17 +88,17 @@ public class CodingBat {
         }
         return sum;
     }
-    //Array-2 fizzArray
-    public int[] fizzArray(int n) {
-        int[] x = new int[n];
-
-        for(int i = 0; i < n; i++)
-            x[i] = i;
-
-        return x;
+    //Array-2 lucky13
+    public static boolean lucky13(int[] nums) {
+        boolean x = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1 || nums[i] == 3) {
+                x = false;
+            }
+        } return x;
     }
     //Array-3 maxSpan
-    public int maxSpan(int[] nums) {
+    public static int maxSpan(int[] nums) {
         int x = 0;
         for(int i = 0; i < nums.length; i++) {
             int y = nums.length - 1;
